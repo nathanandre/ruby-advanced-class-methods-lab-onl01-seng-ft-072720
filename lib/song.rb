878e7f0de558aa320619831e588233a940bb3c42
Nathan Burns <nathanandreburns@gmail.com>
2020-08-09 01:31:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -46,7 +46,7 @@ class Song
     split_filename = filename.chomp(".mp3").split(" - ")
     song = Song.new
     song.name = split_filename[1]
-    
+    song.artist_name = split_filename[0]
  end
 
 end
